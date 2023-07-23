6bf77352796f8265bcb905356a61c5aa21699855
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-24 01:30:45 +0530

    Username route completed

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,72 @@
-export default function PostPage( { } ){
-    return(
-        <main>
-            
-        </main>
-    )
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+import HeartButton from '../../components/HeartButton';
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import AuthCheck from '@/components/AuthCheck';
+import Link from 'next/link';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+  let post;
+  let path;
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup('posts').get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  return (
+    <main className={styles.container}>
+
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 👍</strong>
+        </p>
+
+        <AuthCheck fallback={
+          <Link href="/enter">
+            <button>Sign Up</button>
+          </Link>
+        }>
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
+
+      </aside>
+    </main>
+  );
 }
