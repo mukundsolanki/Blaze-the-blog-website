d13c589c46a80137532b726ca62f004360049715
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-24 01:37:58 +0530

    Completed the HomePage route

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,62 @@
-import Head from "next/head";
-import styles from '../styles/Home.module.css';
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
+import { useState } from 'react';
 
-export default function Home(){
-    return (
-        <div>
-        
-        </div>
-    )
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
+  return (
+      <main>
+        <PostFeed posts={posts} />
+
+        {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+        <Loader show={loading} />
+
+        {postsEnd && 'You have all caught up!'}
+      </main>
+  );
 }
