4ea9f3b42e461a2bd6d2b2c6fa05226d556d7928
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-23 23:03:55 +0530

    Added custom hooks and unique usernames

--- pages/enter.js
+++ pages/enter.js
@@ -1,18 +1,16 @@
-import { auth, googleAuthProvider } from '../lib/firebase';
-import { useContext } from 'react';
+import { auth, firestore, googleAuthProvider } from '../lib/firebase';
 import { UserContext } from '../lib/context';
+// import Metatags from '@components/Metatags';
+
+import { useEffect, useState, useCallback, useContext } from 'react';
+import debounce from 'lodash.debounce';
 
 export default function Enter(props) {
-
-  const { user, username } = useContext(UserContext)
-
+  const { user, username } = useContext(UserContext);
   return (
     <main>
-      {user ? 
-        !username ? <UsernameForm /> : <SignOutButton /> 
-        : 
-        <SignInButton />
-      }
+      {/* <Metatags title="Enter" description="Sign up for this amazing app!" /> */}
+      {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}
     </main>
   );
 }
@@ -23,9 +21,14 @@ function SignInButton() {
   };
 
   return (
-    <button className="btn-google" onClick={signInWithGoogle}>
-      <img src={'/google.png'} /> Sign in with Google
-    </button>
+    <>
+      <button className="btn-google" onClick={signInWithGoogle}>
+        <img src={'/google.png'} width="30px" /> Sign in with Google
+      </button>
+      <button onClick={() => auth.signInAnonymously()}>
+        Sign in Anonymously
+      </button>
+    </>
   );
 }
 
@@ -34,5 +37,92 @@ function SignOutButton() {
 }
 
 function UsernameForm() {
-  return null;
+  const [formValue, setFormValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    const batch = firestore.batch();
+    batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user.displayName });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  const onChange = (e) => {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log('Firestore read executed!');
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input name="username" placeholder="myname" value={formValue} onChange={onChange} />
+          <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+
+          <h3>Debug State</h3>
+          <div>
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
+            Username Valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available✅</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">Try another username</p>;
+  } else {
+    return <p></p>;
+  }
 }
