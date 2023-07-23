34f03d746bce172ed0fb8273fa56dc21f012f0b3
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-23 22:06:43 +0530

    Created the Navbar and added routing functionality

---
+++ components/Navbar.js
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+
+export default function Navbar() {
+
+  const { user, username } = useContext(UserContext)
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
