b259d75934fed674bf66c65a3ad291f86d1bc340
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-23 22:09:35 +0530

    Resolved the firebase undefined error

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,38 @@
-export default function EnterPage( { } ){
-    return(
-        <main>
-            <h1>Sign UP</h1>
-        </main>
-    )
+import { auth, googleAuthProvider } from '../lib/firebase';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+
+export default function Enter(props) {
+
+  const { user, username } = useContext(UserContext)
+
+  return (
+    <main>
+      {user ? 
+        !username ? <UsernameForm /> : <SignOutButton /> 
+        : 
+        <SignInButton />
+      }
+    </main>
+  );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
